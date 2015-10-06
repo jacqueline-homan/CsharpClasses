71f0975c4f64b67ad47898497c3d178bdfb60b2b
Jacqueline Homan <jacquelinehoman7@gmail.com>
2015-10-05 20:58:40 -0400

    added attributes to Dog class

--- Dog.cs
+++ Dog.cs
@@ -7,6 +7,18 @@ namespace Critters
 		public string name;
 		public string owner;
 
+		public string Owner 
+		{
+			get {return this.owner;}
+			set { this.owner = value; }
+		}
+
+		public string Name 
+		{
+			get {return this.name;}
+			set { this.name = value; }
+		}
+
 		public override void move()
 		{
 			//base.move ();
